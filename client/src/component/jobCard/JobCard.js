eb8bd48edc8b01d982e43111968c680772009249
rajatshuklaofficial <rajatshuklaofficial@gmail.comgit config --global user.email rajatshuklaofficial@gmail.com>
2021-02-07 00:52:04 +0530

    added job card to frontend code

---
+++ client/src/component/jobCard/JobCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css'
+
+
+
+
+const JobCard = (props) =>{
+	return(
+		<div className = 'card'>
+			Yeah job card boy.
+		</div>
+		)
+}
+export default JobCard;
--- client/src/component/layout/Landing.js
+++ client/src/component/layout/Landing.js
@@ -1,4 +1,5 @@
 import React ,{Component} from 'react';
+import JobCard from './../jobCard/JobCard'
 import {Link} from 'react-router-dom';
 
 class Landing extends Component{
@@ -9,12 +10,11 @@ class Landing extends Component{
 		      <div className="container">
 		        <div className="row">
 		          <div className="col-md-12 text-center">
-		            <h1 className="display-3 mb-4">Developer Connector
+		            <h1 className="display-3 mb-4">Rozgar Info
 		            </h1>
-		            <p className="lead"> Create a developer profile/portfolio, share posts and get help from other developers</p>
-		            <hr />
-		            <Link to="/register" className="btn btn-lg btn-info mr-2">Sign Up</Link>
-		            <Link to="/login" className="btn btn-lg btn-light">Login</Link>
+		            <div style={{textAlign:"center"}}>
+		            <JobCard />
+		            </div>
 		          </div>
 		        </div>
 		      </div>
